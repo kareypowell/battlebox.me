71a262ba08040cc5b4666c4a4b8eecdaec11d4fd
Karey Powell <kareyp9@gmail.com>
2013-08-11 00:34:07 -0400

    Remove application secret from repo

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,19 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Battlebox::Application.config.secret_token = '41ccc0cdeb5792352236f28bcf50932dc20bac0d1823030870bc2b6e889a8c31efb15f9b8363eff384cf1ae3ba8e681294b8dc817c17cd5513ebb05fdde6b8f4'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Battlebox::Application.config.secret_token = secure_token
